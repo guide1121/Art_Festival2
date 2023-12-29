075beee1ac9dbcdc6a2ba4124110002b0d0dfa4f
guide1121 <99393112+guide1121@users.noreply.github.com>
2023-12-30 01:01:19 +0700

    fix some nav things

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var navbar = $("#navbar");
+  
+    $(window).scroll(function() {
+      // Add the 'fixed' class to the navbar when the user scrolls down
+      if ($(window).scrollTop() > navbar.offset().top) {
+        navbar.addClass("fixed");
+      } else {
+        navbar.removeClass("fixed");
+      }
+    });
+  });
