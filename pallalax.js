8a316a9b1bf311d2f48bb5d8bf4f63d1cea7dca0
guide1121 <99393112+guide1121@users.noreply.github.com>
2024-01-17 11:34:20 +0700

    Update pallalax.js

--- pallalax.js
+++ pallalax.js
@@ -11,13 +11,32 @@ let rock = document.getElementById('rock');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    castle.style.marginTop = value * 0.027 + 'vw';
-    text.style.marginTop = value * 0.035 + 'vw';
-    fox.style.marginTop = value * 0.02 + 'vw';
+    castle.style.marginTop = value * 0.030 + 'vw';
+    castle.style.width = 100 - value * 0.01 + 'vw';
+    castle.style.height = 140 - value * 0.01 + 'vw';
+    castle.style.left = value * 0.005 + 'vw';
+    text.style.marginTop = value * 0.040 + 'vw';
+    text.style.width = 80 - value * 0.01 + 'vw';
+    text.style.height = 55 - value * 0.01 + 'vw';
+    text.style.left = 8.8 + value * 0.0055 + 'vw';
+
+
+    fox.style.marginTop = value * 0.025 + 'vw';
+    fox.style.width =  115 - value * 0.01 + 'vw';
+    fox.style.height = 160 - value * 0.01 + 'vw';
+    fox.style.left =  value * 0.0075 + 'vw';
+
+
     mermaid.style.marginTop = value * 0.025 + 'vw';
     fairy.style.marginTop = value * 0.025 + 'vw';
-    witch.style.marginTop = value * 0.024 + 'vw';
-    bird.style.marginTop = value * 0.02 + 'vw';
+    witch.style.marginTop = value * 0.025 + 'vw';
+
+
+
+    bird.style.marginTop = value * 0.030 + 'vw';
+    bird.style.width = 85 - value * 0.01 + 'vw';
+    bird.style.height = 115 - value * 0.01 + 'vw';
+    bird.style.left = 10 + value * 0.0055 + 'vw';
     bg.style.filter = 'blur('+ value / 600 +'px)';
 });
 
