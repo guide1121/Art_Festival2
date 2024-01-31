68ad9ed4017f57c6af0af49b6743f7d5b26d48d0
guide1121 <99393112+guide1121@users.noreply.github.com>
2024-01-31 09:29:52 +0700

    fix size

--- oeuvre/index.js
+++ oeuvre/index.js
@@ -1,9 +1,6 @@
-$(document).ready(() => {
-
-  // Declare 'data' variable to store the fetched JSON data
+$(document).ready(async () => {
   let data;
 
-  // Function to create Image elements and load images dynamically
   function loadImage(url) {
     return new Promise((resolve, reject) => {
       var img = new Image();
@@ -12,58 +9,49 @@ $(document).ready(() => {
       img.onerror = reject;
     });
   }
-  // Display loading indicator
+
   $("#loading-indicator").show();
 
-  // Fetch JSON data from a file
-  fetch('https://script.google.com/macros/s/AKfycbwBbgnWdNmcw4oppVXrmWeN5t-6sRYmw6Npb3SI6v4I2aCpjxqwC4xaU1YBJkiecGFv/exec')
-    .then(response => response.json())
-    .then(async jsonData => {
-      // Store the JSON data in the 'data' variable
-      data = jsonData;
+  try {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwBbgnWdNmcw4oppVXrmWeN5t-6sRYmw6Npb3SI6v4I2aCpjxqwC4xaU1YBJkiecGFv/exec');
+    const jsonData = await response.json();
+    data = jsonData;
 
-      // Process the JSON data
-      var html = '';
-      for (let i = 0; i < data.length; i++) {
-        const fileId = extractFileId(data[i]['ผลงาน']);
-        const imageUrl = `https://lh3.googleusercontent.com/d/${fileId}?authuser=0`;
+    const loadImagePromises = data.map(async (item, i) => {
+      const fileId = extractFileId(item['ผลงาน']);
+      const imageUrl = `https://lh3.googleusercontent.com/d/${fileId}?authuser=0`;
+      const imgElement = await loadImage(imageUrl);
+      const imgWidth = imgElement.width;
+      const imgHeight = imgElement.height;
 
-        // Load the image dynamically to get its dimensions
-        const imgElement = await loadImage(imageUrl);
-        const imgWidth = imgElement.width;
-        const imgHeight = imgElement.height;
-
-        // Set a maximum width for the image containers
-        const maxWidth = 250; // Adjust this value as needed
-        
-
-        html += `<div onclick="showDetails(${i})"
-                    class="individual ${data[i].ห้อง}">
+      const maxWidth = 250;
+      return `<div onclick="showDetails(${i})"
+                    class="individual ${item.ห้อง}">
                     <img class="artwork-img" src="${imageUrl}" alt="" onload="resizeImage(this)">
-                  <p style="font-size: 2vw;font-weight: 700;" class = "artwork-name">${data[i].ชื่อผลงาน}</p>
-                  <p style="font-size: 1vw;">${data[i].ชื่อนามสกุล}</p>
+                  <p style="font-size: 2vw;font-weight: 700;" class="artwork-name">${item.ชื่อผลงาน}</p>
+                  <p style="font-size: 1vw;">${item.ชื่อนามสกุล}</p>
                 </div>`;
-      }
+    });
 
-      $("#artworklist").html(html);
-      // Hide loading indicator after data is loaded
-      $("#loading-indicator").hide();
-    })
-    .catch(error => {
-      // Hide loading indicator in case of an error
-      $("#loading-indicator").hide();
-      console.error('Error fetching JSON:', error);
-  });
+    const htmlArray = await Promise.all(loadImagePromises);
+    const html = htmlArray.join('');
 
-    function extractFileId(url) {
-      const match = url.match(/\/d\/(.+?)\/|\/open\?id=(.+?)$/);
-      return match ? match[1] || match[2] : null;
-    }
+    $("#artworklist").html(html);
+    $("#loading-indicator").hide();
+  } catch (error) {
+    $("#loading-indicator").hide();
+    console.error('Error fetching JSON:', error);
+  }
+
+  function extractFileId(url) {
+    const match = url.match(/\/d\/(.+?)\/|\/open\?id=(.+?)$/);
+    return match ? match[1] || match[2] : null;
+  }
 
   function showDetails(index) {
     var selectedArtwork = data[index];
     var imageUrl = `https://lh3.googleusercontent.com/d/${extractFileId(selectedArtwork['ผลงาน'])}?authuser=0`;
-    // Your existing showDetails function remains unchanged
+
     $(".modal img").attr("src", imageUrl);
     $(".modal p:nth-child(1)").text(selectedArtwork.ชื่อผลงาน);
     $(".modal p:nth-child(2)").text(selectedArtwork.ชื่อนามสกุล);
@@ -71,8 +59,9 @@ $(document).ready(() => {
     $(".modal").css("display", "block");
   }
 
-  window.showDetails = showDetails; // Expose the function globally
+  window.showDetails = showDetails;
 });
+
 
 function resizeImage(img) {
   const container = img.parentElement;
